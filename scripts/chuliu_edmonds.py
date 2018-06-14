7b4e22c82eda30669cf753a58fb51e74f958eadb
Tim Dozat <tdozat@gmail.com>
2018-06-14 03:55:22 -0400

    Fixed chu-liu/edmonds

--- scripts/chuliu_edmonds.py
+++ scripts/chuliu_edmonds.py
@@ -131,8 +131,13 @@ def chuliu_edmonds_one_root(scores):
   roots = np.where(np.equal(tree[1:], 0))[0]+1
   if len(roots) == 1:
     return tree
+  elif len(roots) == 0:
+    root = np.argmax(scores[0,:])
+    scores[root] = 0
+    scores[root,0] = 1
+    return chuliu_edmonds(scores)
   else:
-    best_score, best_tree = -np.inf, []
+    best_score, best_tree = -np.inf, None
     for root in roots:
       _scores = np.array(scores)
       _scores[:,0] = 0
